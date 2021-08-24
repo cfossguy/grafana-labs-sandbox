5514056d2b59d7575a2940740572455ada6b3005
James Williams <james.williams@grafana.com>
2021-08-24 08:02:54 -0700

    fixed loki config

--- k6/script.js
+++ k6/script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://34.133.181.168/slow/');
-  http.get('http://34.133.181.168/fast/');
+  http.get('http://35.226.45.191/slow/');
+  http.get('http://35.226.45.191/fast/');
   sleep(1);
 }
