ca6be4d434f0ee0e892c646fce689d321ffb9a33
James Williams <james.williams@grafana.com>
2021-09-16 17:42:17 -0400

    added k6 cron job

--- k6/script.js
+++ k6/script.js
@@ -1,14 +1,21 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
+export let options = {
+  stages: [
+    { duration: '20s', target: 10 },
+    { duration: '20s', target: 20 },
+    { duration: '20s', target: 30 },
+  ],
+};
+
 export default function () {
   http.get('http://35.226.45.191/fast');
   http.get('http://35.226.45.191/fast');
   http.get('http://35.226.45.191/fast');
-  http.get('http://35.226.45.191/fast');
+  http.get('http://35.226.45.191/roulette');
   http.get('http://35.226.45.191/roulette');
   http.get('http://35.226.45.191/roulette');
   http.get('http://35.226.45.191/slow');
   http.get('http://35.226.45.191/trip/3');
-  sleep(1);
 }
