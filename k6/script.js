a8d46deb8026221ff47f1d999f1124816290f83a
James Williams <james.williams@grafana.com>
2021-09-22 10:15:49 -0400

    load gen tweaks

--- k6/script.js
+++ k6/script.js
@@ -3,14 +3,18 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '20s', target: 20 },
-    { duration: '20s', target: 40 },
-    { duration: '20s', target: 80 },
+    { duration: '20s', target: 10 },
+    { duration: '20s', target: 30 },
+    { duration: '20s', target: 50 },
   ],
 };
 
 export default function () {
   http.get('http://35.226.45.191/fast');
+  http.get('http://35.226.45.191/fast');
+  http.get('http://35.226.45.191/fast');
+  http.get('http://35.226.45.191/fast');
+  http.get('http://35.226.45.191/roulette');
   http.get('http://35.226.45.191/roulette');
   http.get('http://35.226.45.191/slow');
   http.get('http://35.226.45.191/trip/3');
