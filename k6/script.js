15b0f7843ad2cd5c1172add1bdc9d5753aee03ca
James Williams <james.williams@grafana.com>
2021-09-24 20:28:18 -0400

    k6 tweaks

--- k6/script.js
+++ k6/script.js
@@ -3,9 +3,9 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '20s', target: 10 },
-    { duration: '20s', target: 30 },
-    { duration: '20s', target: 50 },
+    { duration: '30s', target: 10 },
+    { duration: '30s', target: 20 },
+    { duration: '30s', target: 30 },
   ],
 };
 
