b9311d39d1f8d05e3e1c985d1404950da943a903
James Williams <james.williams@grafana.com>
2021-09-27 10:04:13 -0400

    k6 duration bumped to 60s

--- k6/script.js
+++ k6/script.js
@@ -3,9 +3,9 @@ import { sleep } from 'k6';
 
 export let options = {
   stages: [
-    { duration: '30s', target: 10 },
-    { duration: '30s', target: 20 },
-    { duration: '30s', target: 30 },
+    { duration: '60s', target: 10 },
+    { duration: '60s', target: 20 },
+    { duration: '60s', target: 30 },
   ],
 };
 
