4a863939fd9a8cfcdfa18cadd36ee0b232c7f422
James Williams <james.williams@grafana.com>
2022-02-04 10:28:42 -0500

    log-demo fixes

--- log-demo/deploy.sh
+++
@@ -1,3 +0,0 @@
-mvn clean package
-gcloud compute scp ./target/log-demo-1.0-SNAPSHOT.war se-jwilliams-tomcat-vm:~/apache-tomcat-8.5.75/webapps --zone=us-central1-a
-gcloud compute scp ./_grafana-agent.yaml se-jwilliams-tomcat-vm:~/grafana-agent.yaml --zone=us-central1-a
