02891cb81b9d5dbb1125fb415698d61125bb8829
James Williams <cfossguy@gmail.com>
2022-02-16 11:55:21 -0500

    k6

--- k6/script.js
+++ k6/script.js
@@ -12,20 +12,26 @@ export let options = {
 };
 
 export default function () {
-  // 4 fast calls with 10kb and 20kb response payloads
+  // 10 fast calls with 10kb and 20kb response payloads
   http.get('http://34.134.135.13/fast/10');
   http.get('http://34.134.135.13/fast/20');
   http.get('http://34.134.135.13/fast/10');
   http.get('http://34.134.135.13/fast/20');
+  http.get('http://34.134.135.13/fast/10');
+  http.get('http://34.134.135.13/fast/10');
+  http.get('http://34.134.135.13/fast/10');
+  http.get('http://34.134.135.13/fast/10');
+  http.get('http://34.134.135.13/fast/10');
+  http.get('http://34.134.135.13/fast/10');
 
   // 3 roulette calls will fail 1 out of 10
   http.get('http://34.134.135.13/roulette/10');
   http.get('http://34.134.135.13/roulette/10');
   http.get('http://34.134.135.13/roulette/10');
 
+
   // 2 slow calls with 2 second sleep times and 50kb response payloads
   http.get('http://34.134.135.13/slow/3/20');
-  http.get('http://34.134.135.13/slow/3/10');
 
   // 2 round trip call with 3 second sleep time
   http.get('http://34.134.135.13/trip/2/2/10');
